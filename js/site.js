931179f946e68403358dffb684f01d4e462a1784
Pespino22 <112427199+Pespino22@users.noreply.github.com>
2023-03-23 12:52:15 -0500

    adding missing parts

--- js/site.js
+++ js/site.js
@@ -3,3 +3,19 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+const button =
+document.querySelector ('.button');
+const submit =
+document.querySelector ('.submit');
+
+function toggleClass() {
+  this.classList.toogle('active');
+}
+
+function addClass() {
+  this.class.add('finished');
+}
+button.addEventListener('click', toggleClass);
+button.addEventListener('transitionend', toggleClass);
+button.addEventListener('transitionend', addClass);
